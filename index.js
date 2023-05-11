e304164b1409432c80b702185a6b938ba7f4f424
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-12 05:40:58 +0600

    adding delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -44,6 +44,14 @@ async function run() {
         const result = await coffeeCollection.insertOne(coffee)
         // console.log(coffee);
         res.send(result)
+
+    })
+    app.delete('/coffee/:id', async (req,res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = coffeeCollection.deleteOne(query);
+      res.send(result)
 
     })
     // Send a ping to confirm a successful connection
