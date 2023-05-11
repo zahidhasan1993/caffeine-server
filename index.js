552234e93abf4b79aa9b160304ef878050ef642b
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-11 14:54:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+//routes and mongodb connections
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.d09ztu7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res) => {
+    res.send('welcome to caffeine server site');
+})
+
+app.listen(port, () => {
+    console.log('this server is running on port', port);
+})
