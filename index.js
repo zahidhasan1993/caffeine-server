29fe6b89697630d71b45864c6a2a4383ca9bfa19
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-11 17:12:22 +0600

    connection to database to upload new coffee and adding post method

--- index.js
+++ index.js
@@ -31,12 +31,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const coffeeCollection = client.db("coffeeDB").collection('newCoffee');
+    //Db and routes connections
+    app.post('/coffee',async (req,res) => {
+        const coffee = req.body;
+        const result = await coffeeCollection.insertOne(coffee)
+        // console.log(coffee);
+        res.send(result)
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
